b0c69a5169beea1ab0ae34512809e528ae8cf4f1
park.jihoon <park.jihoon@aiv.ai>
2024-06-27 21:34:06 +0900

    fix: docker access

--- Kafka-Docker/pizza-service/app.py
+++ Kafka-Docker/pizza-service/app.py
@@ -1,18 +1,20 @@
+import pizza_service
+import json
+from threading import Thread
+from flask import Flask
 import sys
 sys.path.append('/app')
 
-from flask import Flask
-from threading import Thread
-import json
-import pizza_service
 
 app = Flask(__name__)
+
 
 @app.route('/order/<count>', methods=['POST'])
 def order_pizzas(count):
     print('@@@@')
     order_id = pizza_service.order_pizzas(int(count))
-    return json.dumps({"order_id": order_id})
+    return json.dumpqs({"order_id": order_id})
+
 
 @app.route('/order/<order_id>', methods=['GET'])
 def get_order(order_id):
@@ -20,8 +22,10 @@ def get_order(order_id):
 
     return pizza_service.get_order(order_id)
 
+
 if __name__ == '__main__':
-    app.run(None,4000)
+    app.run("0.0.0.0", 4000)
+
 
 @app.before_first_request
 def launch_consumer():
