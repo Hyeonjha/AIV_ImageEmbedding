17ac5fc61b0d5d7237fba5cc85e75d77733722c5
Hyeonjha <olivia82597@gmail.com>
2024-07-02 16:06:45 +0900

    delete Issue9 folder

--- Issue9/embedding-service/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:9092')
-    WEAVIATE_URL = os.getenv('WEAVIATE_URL', 'http://weaviate:8080')
-    IMAGE_FOLDER = '/mnt/data/images'
--- Issue9/image-service/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://user:password@db:5432/imagedb')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:9092')
